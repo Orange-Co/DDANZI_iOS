1a1bc3c1afe25adb5ec047bc5e22942608436c38
Zoe <mam07065@naver.com>
2024-06-15 11:41:39 +0900

    [Feat/#3] codebase Setting

--- DDANZI_iOS/DDANZI_iOS/Presentation/Common/CustomNavigationBarView.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Common/CustomNavigationBarView.swift
@@ -5,4 +5,36 @@
 //  Created by 이지희 on 6/15/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class CustomNavigationBarView: UIView {
+    // MARK: - componenets
+    
+    
+    // MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        
+    }
+    
+    private func setConstraints() {
+        
+    }
+    
+    
+}
--- DDANZI_iOS/DDANZI_iOS/Presentation/Home/HomeView.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Home/HomeView.swift
@@ -5,4 +5,31 @@
 //  Created by 이지희 on 6/15/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class HomeView : UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        
+    }
+    
+    private func setConstraints() {
+        
+    }
+}
