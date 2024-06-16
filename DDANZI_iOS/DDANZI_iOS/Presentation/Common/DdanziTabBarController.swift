453edfe349b0d023e62817246d144015e393a44e
Zoe <mam07065@naver.com>
2024-06-16 22:40:51 +0900

    [Feat/#5] custom TabBarController

--- DDANZI_iOS/DDANZI_iOS/Presentation/Common/DdanziTabBarController.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Common/DdanziTabBarController.swift
@@ -8,22 +8,89 @@
 import UIKit
 
 class DdanziTabBarController: UITabBarController {
+    private var tabs: [UIViewController] = []
+    private let tabBarHeight: CGFloat = 100
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let homeViewController = HomeViewController()
+    let mypageViewController  = HomeViewController()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.delegate = self
+        self.navigationController?.navigationBar.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        var tabBarFrame = self.tabBar.frame
+        tabBarFrame.size.height = tabBarHeight
+        tabBarFrame.origin.y = self.view.frame.size.height - tabBarHeight
+        self.tabBar.frame = tabBarFrame
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTabBarItems()
+        setTabBarAppearance()
+    }
+    
+    func setTabBarItems() {
+        tabs = [
+            UINavigationController(rootViewController: homeViewController),
+            UINavigationController(rootViewController: mypageViewController)
+        ]
+        
+        TabBarItem.allCases.forEach {
+            tabs[$0.rawValue].tabBarItem = $0.asTabBarItem()
+            tabs[$0.rawValue].tabBarItem.tag = $0.rawValue
+        }
+        
+        setViewControllers(tabs, animated: true)
+    }
+    
+    func setTabBarAppearance() {
+        tabBar.backgroundColor = .black
+        tabBar.tintColor = .white
+        tabBar.barTintColor = .black
+    
+        tabBar.roundCorners(cornerRadius: 10, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        
+        let myFont = UIFont(name: "Pretendard-Bold", size: 10.0)!
+        let fontAttributes = [NSAttributedString.Key.font: myFont]
+        UITabBarItem.appearance().setTitleTextAttributes(fontAttributes, for: .normal)
+        
+    }
+}
 
+extension DdanziTabBarController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+        if let selectedViewController = tabBarController.selectedViewController {
+            let myFont = UIFont(name: "Pretendard-Bold", size: 10.0)!
+            let selectedFontAttributes = [NSAttributedString.Key.font: myFont]
+            selectedViewController.tabBarItem.setTitleTextAttributes(selectedFontAttributes, for: .normal)
+        }
+        
+        for (index, controller) in tabBarController.viewControllers!.enumerated() {
+            if let tabBarItem = controller.tabBarItem {
+                if index != tabBarController.selectedIndex {
+                    let myFont = UIFont(name: "Pretendard-Medium", size: 10.0)!
+                    let defaultFontAttributes = [NSAttributedString.Key.font: myFont]
+                    tabBarItem.setTitleTextAttributes(defaultFontAttributes, for: .normal)
+                }
+            }
+        }
+        
+        let selectedIndex = tabBarController.selectedIndex
+        switch selectedIndex {
+        case 0:
+            tabBar.items?[0].imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        case 1:
+            tabBar.items?[0].imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        default:
+            tabBar.items?[0].imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
+    }
+    
 }
--- DDANZI_iOS/DDANZI_iOS/Presentation/Common/DdanziTabBarItems.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Common/DdanziTabBarItems.swift
@@ -5,4 +5,55 @@
 //  Created by 이지희 on 6/16/24.
 //
 
-import Foundation
+import UIKit
+
+enum TabBarItem: Int, CaseIterable {
+    case home
+    case profile
+}
+
+extension TabBarItem {
+    var title: String {
+        switch self {
+        case .home:
+            return "홈"
+        case .profile:
+            return "프로필"
+        }
+    }
+}
+
+extension TabBarItem {
+    var Icon: UIImage? {
+        switch self {
+        case .home:
+            return .icHomeUnselect
+        case .profile:
+            return .icMypageEmpty
+        }
+    }
+    
+    var selectedIcon: UIImage? {
+        switch self {
+        case .home:
+            return .icHome
+        case .profile:
+            return .icMypage
+        }
+    }
+}
+
+extension TabBarItem {
+    public func asTabBarItem() -> UITabBarItem {
+        let tabBarItem = UITabBarItem(
+            title: title,
+            image: Icon,
+            selectedImage: selectedIcon
+        )
+        
+        tabBarItem.imageInsets = UIEdgeInsets(top: -5, left: 0, bottom: 0, right: 0)
+        
+        return tabBarItem
+    }
+}
+
