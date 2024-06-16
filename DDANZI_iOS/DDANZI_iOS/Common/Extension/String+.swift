854c31040970a29316037b178d26b2e7f6cf07ac
Zoe <mam07065@naver.com>
2024-06-16 22:42:15 +0900

    [Add/#5] add String Extension

--- DDANZI_iOS/DDANZI_iOS/Common/Extension/String+.swift
+++ DDANZI_iOS/DDANZI_iOS/Common/Extension/String+.swift
@@ -5,4 +5,12 @@
 //  Created by 이지희 on 6/16/24.
 //
 
-import Foundation
+import UIKit
+
+extension String {
+    func strikeThrough() -> NSAttributedString {
+           let attributeString = NSMutableAttributedString(string: self)
+           attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+           return attributeString
+       }
+}
