cd3ed5c53a6bfce801ce0ad8d49b23b3ce67341c
Zoe <mam07065@naver.com>
2024-06-18 03:02:13 +0900

    [Feat/#7] 1st Product Detail UI

--- DDANZI_iOS/DDANZI_iOS/Presentation/ProductDetail/ViewControllers/ProductDetailViewController.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/ProductDetail/ViewControllers/ProductDetailViewController.swift
@@ -7,23 +7,157 @@
 
 import UIKit
 
+import ReactorKit
+import RxCocoa
+import RxDataSources
+import RxSwift
+import SnapKit
+import Then
+
 class ProductDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: Properties
+    let dummy = ProductDetailModel(productTitle: "퓨어 오일 퍼퓸 10ml 입니다 긴제목은 어떻게 될까요? 짧은 것도 이간격",
+                                   discountRate: 30, price: 48900, beforePrice: 54000,
+                                   remainAmount: 30)
+    // MARK: Compenets
+    private let customNavigationBar = CustomNavigationBarView(navigationBarType: .home)
+    
+    private let productImageView = UIImageView().then {
+        $0.backgroundColor = .gray2
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let labelView = UIView() // TO DO: 라벨뷰 커스텀
+    
+    private let productTitleLabel = UILabel().then {
+        $0.font = .title4Sb24
+        $0.textColor = .black
+        $0.numberOfLines = 2
     }
-    */
-
+    
+    private let discountLabel = UILabel().then {
+        $0.font = .title3Sb28
+        $0.textColor = .black
+    }
+    
+    private let priceLabel = UILabel().then {
+        $0.font = .title3Sb28
+        $0.textColor = .black
+    }
+    
+    private let beforePriceLabel = UILabel().then {
+        $0.font = .body1B20
+        $0.textColor = .gray2
+    }
+    
+    private let remainLabel = UILabel().then {
+        $0.text = StringLiterals.ProductDetail.Label.remainLabel
+        $0.font = .body3Sb16
+        $0.textColor = .black
+    }
+    
+    private let amountLabel = UILabel().then {
+        $0.font = .body3Sb16
+        $0.textColor = .gray3
+    }
+    
+    private let moreLinkButton = UIButton().then {
+        $0.setTitle(StringLiterals.ProductDetail.Button.morelinkButtonText,
+                    for: .normal)
+        $0.setTitleColor(.gray3, for: .normal)
+        $0.titleLabel?.font = .body3Sb16
+    }
+    
+    
+    // MARK: LifeCycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = .white
+        bindDummy()
+        setUI()
+    }
+    
+    // MARK: LayoutHelper
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func bindDummy() {
+        productTitleLabel.text = dummy.productTitle
+        discountLabel.text = "\(dummy.discountRate)%"
+        priceLabel.text = "\(dummy.price)"
+        beforePriceLabel.attributedText = "\(dummy.beforePrice)".strikeThrough()
+        
+        amountLabel.text = "\(dummy.remainAmount)개"
+        
+    }
+    
+    private func setHierarchy() {
+        view.addSubviews(customNavigationBar,
+                         productImageView,
+                         labelView,
+                         productTitleLabel,
+                         discountLabel,
+                         priceLabel,
+                         beforePriceLabel,remainLabel,
+                         amountLabel,
+                         moreLinkButton)
+    }
+    
+    private func setConstraints() {
+        customNavigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets.top)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        productImageView.snp.makeConstraints {
+            $0.top.equalTo(customNavigationBar.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(375)
+        }
+        
+        labelView.snp.makeConstraints {
+            $0.top.equalTo(productImageView.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(20)
+            $0.height.equalTo(25)
+        }
+        
+        productTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(labelView.snp.bottom).offset(15)
+            $0.leading.trailing.equalToSuperview().inset(21)
+            $0.height.equalTo(58)
+        }
+        
+        discountLabel.snp.makeConstraints {
+            $0.top.equalTo(productTitleLabel.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(21)
+        }
+        
+        beforePriceLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalTo(discountLabel.snp.bottom)
+        }
+        
+        priceLabel.snp.makeConstraints {
+            $0.trailing.equalTo(beforePriceLabel.snp.leading).offset(-8)
+            $0.bottom.equalTo(discountLabel.snp.bottom)
+        }
+        
+        remainLabel.snp.makeConstraints {
+            $0.top.equalTo(priceLabel.snp.bottom).offset(15)
+            $0.leading.equalToSuperview().offset(21)
+        }
+        
+        amountLabel.snp.makeConstraints {
+            $0.leading.equalTo(remainLabel.snp.trailing).offset(8)
+            $0.centerY.equalTo(remainLabel.snp.centerY)
+        }
+        
+        moreLinkButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.top.equalTo(remainLabel.snp.bottom).offset(15)
+        }
+    }
+    
 }
