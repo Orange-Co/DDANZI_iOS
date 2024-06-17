16bae22e1bb0e36e2b880f4e855f3e1cd7a8e970
Zoe <mam07065@naver.com>
2024-06-18 02:59:58 +0900

    [Feat/#7] Foldering

---
+++ DDANZI_iOS/DDANZI_iOS/Common/Resource/Literals/String.swift
@@ -0,0 +1,9 @@
+//
+//  String.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 6/18/24.
+//
+
+import Foundation
+
---
+++ DDANZI_iOS/DDANZI_iOS/Presentation/ProductDetail/Model/ProductDetailModel.swift
@@ -0,0 +1,16 @@
+//
+//  ProductModel.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 6/18/24.
+//
+
+import Foundation
+
+struct ProductDetailModel {
+    let productTitle: String
+    let discountRate: Int
+    let price: Int
+    let beforePrice: Int
+    let remainAmount: Int
+}
