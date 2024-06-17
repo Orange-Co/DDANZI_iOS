d94984334de29fbf9fa0c94827a349f98d8aef8e
Zoe <mam07065@naver.com>
2024-06-18 03:00:37 +0900

    [Feat/#7] StringLiterals  apply

--- DDANZI_iOS/DDANZI_iOS/Common/Resource/Literals/String.swift
+++ DDANZI_iOS/DDANZI_iOS/Common/Resource/Literals/String.swift
@@ -7,3 +7,21 @@
 
 import Foundation
 
+enum StringLiterals {
+    
+    enum TabBar {
+        enum ItemTitle {
+            static let home = "홈"
+            static let myPage = "마이페이지"
+        }
+    }
+    
+    enum ProductDetail {
+        enum Label {
+            static let remainLabel = "남은 재고"
+        }
+        enum Button{
+            static let morelinkButtonText = "이 상품에 대한 자세한 정보를 보고 싶다면?"
+        }
+    }
+}
