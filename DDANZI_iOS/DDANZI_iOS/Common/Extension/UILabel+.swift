89067dff5a8914aa4128020cab956eccc9b2a930
Zoe <mam07065@naver.com>
2024-06-20 00:46:06 +0900

    [Add/#5] add Label Extension

--- DDANZI_iOS/DDANZI_iOS/Common/Extension/UILabel+.swift
+++ DDANZI_iOS/DDANZI_iOS/Common/Extension/UILabel+.swift
@@ -5,4 +5,54 @@
 //  Created by 이지희 on 6/19/24.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    
+    func setTextColor(_ color: UIColor, range: NSRange) {
+        guard let attributedString = self.mutableAttributedString() else { return }
+        
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: color, range: range)
+        self.attributedText = attributedString
+    }
+    
+    
+    func setBoldFont(_ boldFontName: String, range: NSRange) {
+        guard let font = self.font,
+              let boldFont = UIFont(name: boldFontName, size: font.pointSize) else {
+            return
+        }
+        
+        return setFont(boldFont, range: range)
+    }
+    
+    func setFont(_ font: UIFont, range: NSRange) {
+        guard let attributedString = self.mutableAttributedString() else { return }
+        
+        attributedString.addAttribute(NSAttributedString.Key.font, value: font, range: range)
+        self.attributedText = attributedString
+    }
+    
+    func setUnderline(range: NSRange) {
+        
+        guard let attributedString = self.mutableAttributedString() else { return }
+        
+        attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: range)
+        self.attributedText = attributedString
+    }
+    
+    private func mutableAttributedString() -> NSMutableAttributedString? {
+        guard let labelText = self.text, let labelFont = self.font else { return nil }
+        
+        var attributedString: NSMutableAttributedString?
+        if let attributedText = self.attributedText {
+            attributedString = attributedText.mutableCopy() as? NSMutableAttributedString
+        } else {
+            attributedString = NSMutableAttributedString(string: labelText,
+                                                         attributes: [NSAttributedString.Key.font :labelFont])
+        }
+        
+        return attributedString
+    }
+}
+
