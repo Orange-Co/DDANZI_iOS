2e20b15f6c1386d1f6aa622753a3a438ced3c2f3
Zoe <mam07065@naver.com>
2024-06-23 23:06:09 +0900

    [Feat/#5] 옵션 선택 뷰모델

--- DDANZI_iOS/DDANZI_iOS/Presentation/ProductDetail/OptionSelectViewModel.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/ProductDetail/OptionSelectViewModel.swift
@@ -11,10 +11,16 @@ import RxSwift
 import RxCocoa
 
 final class OptionSelectViewModel {
-    let sections = BehaviorRelay(value: [
-        OptionSectionModel(isExpanded: true, items: ["S","M"]),
-        OptionSectionModel(isExpanded: false, items: ["색상 1", "색상 2"])
-    ])
+    private let disposeBag = DisposeBag()
+    /// BehaviorRelay는 변경 가능한 상태를 나타내며, 기본값을 가질 수 있습니다.
+    let sections = BehaviorRelay<[OptionSectionModel]>(value: [])
+    
+    init() {
+        sections.accept([
+            OptionSectionModel(isExpanded: false, items: ["Item 1", "Item 2"]),
+            OptionSectionModel(isExpanded: false, items: ["Item 3", "Item 4"])
+        ])
+    }
     
     func toggleSection(at index: Int) {
         var currentSections = sections.value
