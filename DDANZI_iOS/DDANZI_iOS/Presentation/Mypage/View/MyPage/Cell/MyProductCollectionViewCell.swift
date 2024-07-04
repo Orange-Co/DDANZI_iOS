8ddd25f1d34b1403ed1a262e744907de51315c82
Zoe <mam07065@naver.com>
2024-07-04 23:27:20 +0900

    [Feat/#11] Mypage Product Cell

---
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Mypage/View/MyPage/Cell/MyProductCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyProductCollectionViewCell.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 7/4/24.
+//
+
+import UIKit
+
+class MyProductCollectionViewCell: UICollectionViewCell {
+    
+}
