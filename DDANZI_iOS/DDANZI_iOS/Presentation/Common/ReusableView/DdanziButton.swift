5e1de347f199e0b777356ffa6b7bc075c8c16072
Zoe <mam07065@naver.com>
2024-07-04 23:18:37 +0900

    [Feat/#11] CustomButton

--- DDANZI_iOS/DDANZI_iOS/Presentation/Common/ReusableView/DdanziButton.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Common/ReusableView/DdanziButton.swift
@@ -5,4 +5,23 @@
 //  Created by 이지희 on 7/4/24.
 //
 
-import Foundation
+import UIKit
+
+final class DdanziButton: UIButton {
+    init (title: String) {
+        super.init(frame: .init(x: 0, y: 0, width: 0, height: 50))
+        configureButton(title: title)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureButton(title: String) {
+        self.backgroundColor = .black
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(.white, for: .normal)
+        self.titleLabel?.font = .body3Sb16
+        self.makeCornerRound(radius: 10)
+    }
+}
