c5b96a0c407d828a4c11259703f97663144ec7d9
Zoe <mam07065@naver.com>
2024-07-09 17:49:44 +0900

    [Feat/#11] Reusable Header Config

--- DDANZI_iOS/DDANZI_iOS/Presentation/Mypage/View/MyPage/HelperView/ProductListHeaderView.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Mypage/View/MyPage/HelperView/ProductListHeaderView.swift
@@ -5,4 +5,62 @@
 //  Created by 이지희 on 7/5/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class ProductListHeaderView: UIView {
+    /// 상품 개수를 나타냅니다.
+    var count = 10
+    
+    // MARK: - UIComponents
+    private let countLabel = UILabel().then {
+        $0.font = .body4R16
+        $0.textColor = .gray3
+    }
+    private let editButton = UIButton().then {
+        $0.setTitle("편집", for: .normal)
+        $0.setUnderline()
+    }
+    
+    // MARK: - Initalizer
+    init(isEditable: Bool) {
+        super.init(frame: .zero)
+        editButton.isHidden = isEditable ? false : true
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Layout Helper
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(countLabel,
+                         editButton)
+    }
+    
+    private func setConstraints() {
+        countLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.centerY.equalToSuperview()
+        }
+        
+        editButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    func setCount(count: Int) {
+        countLabel.text = "총 \(count)개"
+    }
+}
+
