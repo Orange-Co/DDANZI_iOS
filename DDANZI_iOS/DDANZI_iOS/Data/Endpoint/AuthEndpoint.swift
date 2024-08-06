278a3ea2f6f5620b0ebdb35af6c03cef924d5051
Zoe <mam07065@naver.com>
2024-08-07 02:19:35 +0900

    [Feat/#19] AuthEndpoint

--- DDANZI_iOS/DDANZI_iOS/Data/Endpoint/AuthEndpoint.swift
+++ DDANZI_iOS/DDANZI_iOS/Data/Endpoint/AuthEndpoint.swift
@@ -1,5 +1,5 @@
 //
-//  AuthRepository.swift
+//  AuthEndpoint.swift
 //  DDANZI_iOS
 //
 //  Created by 이지희 on 8/2/24.
@@ -9,87 +9,76 @@ import Foundation
 
 import Moya
 
-enum AuthRouter {
-    case socialLogin(data: SocialLoginRequestDTO)
-    case signUp(data: SignUpRequestDTO)
-    case tokenRefresh
-    case revoke
-    case logout
+enum AuthEndpoint {
+  case socialLogin
+  case certification
+  case revoke
+  case logout
 }
 
-extension AuthRouter: BaseTargetType {
-    var headers: Parameters? {
-        switch self {
-        case .socialLogin:
-            return APIConstants.hasSocialTokenHeader
-        case .signUp:
-            return APIConstants.signUpHeader
-        case .tokenRefresh:
-            return APIConstants.hasRefreshTokenHeader
-        case .revoke:
-            return APIConstants.hasTokenHeader
-        case .logout:
-            return APIConstants.hasTokenHeader
-        }
+extension AuthEndpoint: BaseTargetType {
+  var headers: Parameters? {
+    switch self {
+    case .socialLogin:
+      return APIConstants.signUpHeader
+    case .certification:
+      return APIConstants.hasAccessTokenHeader
+    case .revoke:
+      return APIConstants.hasAccessTokenHeader
+    case .logout:
+      return APIConstants.hasAccessTokenHeader
     }
-    
-    var path: String {
-        switch self {
-        case .socialLogin:
-            return "/v1/user/login"
-        case .signUp:
-            return "/v1/user/signup"
-        case .tokenRefresh:
-            return "/v1/user/reissue"
-        case .revoke:
-            return "/v1/user"
-        case .logout:
-            return "/v1/user/logout"
-        }
+  }
+  
+  var path: String {
+    switch self {
+    case .socialLogin:
+      return "/api/v1/auth/signin"
+    case .certification:
+      return "/api/v1/auth/verification"
+    case .revoke:
+      return "/api/v1/auth/withdraw"
+    case .logout:
+      return ""
     }
-    
-    var method: Moya.Method {
-        switch self {
-        case .socialLogin:
-            return .post
-        case .signUp:
-            return .post
-        case .tokenRefresh:
-            return .post
-        case .revoke:
-            return .delete
-        case .logout:
-            return .post
-        }
+  }
+  
+  var method: Moya.Method {
+    switch self {
+    case .socialLogin:
+      return .post
+    case .certification:
+      return .post
+    case .revoke:
+      return .delete
+    case .logout:
+      return .post
     }
-    
-    var task: Moya.Task {
-        switch self {
-        case .socialLogin(let data):
-            return .requestJSONEncodable(data)
-        case .signUp(let data):
-            return .requestJSONEncodable(data)
-        case .tokenRefresh:
-            return .requestPlain
-        case .revoke:
-            return .requestPlain
-        case .logout:
-            return .requestPlain
-        }
+  }
+  
+  var task: Moya.Task {
+    switch self {
+    case .socialLogin:
+      return .requestPlain
+    case .certification:
+      return .requestPlain
+    case .revoke:
+      return .requestPlain
+    case .logout:
+      return .requestPlain
     }
-    
-    var validationType: ValidationType {
-        switch self {
-        case .socialLogin(data:_) :
-            return .none
-        case .signUp(data:_):
-            return .successCodes
-        case .tokenRefresh:
-            return .successCodes
-        case .revoke:
-            return .successCodes
-        case .logout:
-            return .successCodes
-        }
+  }
+  
+  var validationType: ValidationType {
+    switch self {
+    case .socialLogin:
+      return .successCodes
+    case .certification:
+      return .successCodes
+    case .revoke:
+      return .successCodes
+    case .logout:
+      return .successCodes
     }
+  }
 }
