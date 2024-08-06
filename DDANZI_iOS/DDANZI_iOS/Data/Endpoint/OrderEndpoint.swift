4706fd50fdfdb9fa96af0d5eb0e2055bea530021
Zoe <mam07065@naver.com>
2024-08-07 02:22:30 +0900

    [Chore/#19]

--- DDANZI_iOS/DDANZI_iOS/Data/Endpoint/OrderEndpoint.swift
+++ DDANZI_iOS/DDANZI_iOS/Data/Endpoint/OrderEndpoint.swift
@@ -1,8 +1,30 @@
+////
+////  OrderEndpoint.swift
+////  DDANZI_iOS
+////
+////  Created by 이지희 on 8/2/24.
+////
 //
-//  OrderRepository.swift
-//  DDANZI_iOS
+//import Foundation
 //
-//  Created by 이지희 on 8/2/24.
+//import Moya
 //
-
-import Foundation
+//enum OrderEndpoint {
+//}
+//
+//extension OrderEndpoint: BaseTargetType {
+//    var headers: Parameters? {
+//    }
+//    
+//    var path: String {
+//    }
+//    
+//    var method: Moya.Method {
+//    }
+//    
+//    var task: Moya.Task {
+//    }
+//    
+//    var validationType: ValidationType {
+//    }
+//}
