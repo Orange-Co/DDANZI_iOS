d7077386030790be3cc55a7edbb3295d8d429d48
Zoe <mam07065@naver.com>
2024-08-07 02:21:52 +0900

    [Feat/#19] Static Providers

--- DDANZI_iOS/DDANZI_iOS/Data/Network/Provider.swift
+++ DDANZI_iOS/DDANZI_iOS/Data/Network/Provider.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import Moya
+
+struct Providers {
+  static let AuthProvider = NetworkProvider<AuthEndpoint>(withAuth: false)
+  static let HomeProvider = NetworkProvider<HomeEndpoint>(withAuth: true)
+  static let SearchProvider = NetworkProvider<SearchEndpoint>(withAuth: true)
+  static let InterestProvider = NetworkProvider<InterestEndpoint>(withAuth: true)
+  static let MypageProvider = NetworkProvider<MypageEndpoint>(withAuth: true)
+}
+
+extension MoyaProvider {
+  convenience init(withAuth: Bool) {
+    if withAuth {
+      self.init(session: Session(interceptor: AuthInterceptor.shared),
+                plugins: [MoyaLoggingPlugin()])
+    } else {
+      self.init(plugins: [MoyaLoggingPlugin()])
+    }
+  }
+}
