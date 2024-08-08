74153b228b68e2fc90998a760dd76e7d5558423c
Zoe <mam07065@naver.com>
2024-08-09 04:02:04 +0900

    feat: #22 고객 정보 모델

---
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Mypage/Model/UserInfoModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserInfoModel.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 8/8/24.
+//
+
+import Foundation
+
+struct UserInfoModel {
+  let name: String
+  let phone: String
+  let nickName: String
+}
