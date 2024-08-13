23a804f1636d8b8d1e60fae53ae19a0f96d5e44c
Zoe <mam07065@naver.com>
2024-08-14 05:20:06 +0900

    [Feat/#24] 옵션 헤더뷰 구현

---
+++ DDANZI_iOS/DDANZI_iOS/Presentation/ProductDetail/ViewControllers/Cell/OptionHeaderView.swift
@@ -0,0 +1,38 @@
+//
+//  OptionHeaderView.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 8/14/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class OptionSectionHeaderView: UICollectionReusableView {
+  static let identifier = "OptionSectionHeaderView"
+  
+  private let titleLabel = UILabel().then {
+    $0.font = .body3Sb16
+    $0.textColor = .black
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    addSubview(titleLabel)
+    
+    titleLabel.snp.makeConstraints {
+      $0.centerY.equalToSuperview()
+      $0.leading.equalToSuperview().offset(20)
+    }
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func configure(with title: String) {
+    titleLabel.text = title
+  }
+}
