596d1cb19871c0a5f0edfbd82f8fc680247e76db
Zoe <mam07065@naver.com>
2024-08-15 02:15:10 +0900

    [Feat/#26] Local Storage

--- DDANZI_iOS/DDANZI_iOS/Common/LocalStorage/UserDefaltManager.swift
+++ DDANZI_iOS/DDANZI_iOS/Common/LocalStorage/UserDefaltManager.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Foundation
+
+// Enum for UserDefaults keys
+enum UserDefaultsKeyType: String {
+    case refreshToken = "refreshtoken"
+    case nickName = "nickName"
+}
+
+extension UserDefaults {
+    
+    // MARK: - Save Methods
+    
+    func set(_ value: String, forKey key: UserDefaultsKeyType) {
+        set(value, forKey: key.rawValue)
+    }
+    
+    func set(_ value: Bool, forKey key: UserDefaultsKeyType) {
+        set(value, forKey: key.rawValue)
+    }
+    
+    func set(_ value: Int, forKey key: UserDefaultsKeyType) {
+        set(value, forKey: key.rawValue)
+    }
+    
+    // MARK: - Retrieve Methods
+    
+    func string(forKey key: UserDefaultsKeyType) -> String? {
+        return string(forKey: key.rawValue)
+    }
+    
+    func bool(forKey key: UserDefaultsKeyType) -> Bool {
+        return bool(forKey: key.rawValue)
+    }
+    
+    func integer(forKey key: UserDefaultsKeyType) -> Int {
+        return integer(forKey: key.rawValue)
+    }
+    
+    // MARK: - Remove Method
+    
+    func removeObject(forKey key: UserDefaultsKeyType) {
+        removeObject(forKey: key.rawValue)
+    }
+}
