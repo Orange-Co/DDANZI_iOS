d19783dbbfa1b14cee15bb2366aee408b273904b
Zoe <mam07065@naver.com>
2024-08-15 17:15:29 +0900

    [Feat/#26] UserDefaults 사용

--- DDANZI_iOS/DDANZI_iOS/Common/LocalStorage/UserDefaltManager.swift
+++ DDANZI_iOS/DDANZI_iOS/Common/LocalStorage/UserDefaltManager.swift
@@ -10,43 +10,45 @@ import Foundation
 
 // Enum for UserDefaults keys
 enum UserDefaultsKeyType: String {
-    case refreshToken = "refreshtoken"
-    case nickName = "nickName"
+  case refreshToken = "refreshtoken"
+  case accesstoken = "accesstoken"
+  case nickName = "nickName"
+  case portoneAccessToken = "portoneAccessToken"
 }
 
 extension UserDefaults {
-    
-    // MARK: - Save Methods
-    
-    func set(_ value: String, forKey key: UserDefaultsKeyType) {
-        set(value, forKey: key.rawValue)
-    }
-    
-    func set(_ value: Bool, forKey key: UserDefaultsKeyType) {
-        set(value, forKey: key.rawValue)
-    }
-    
-    func set(_ value: Int, forKey key: UserDefaultsKeyType) {
-        set(value, forKey: key.rawValue)
-    }
-    
-    // MARK: - Retrieve Methods
-    
-    func string(forKey key: UserDefaultsKeyType) -> String? {
-        return string(forKey: key.rawValue)
-    }
-    
-    func bool(forKey key: UserDefaultsKeyType) -> Bool {
-        return bool(forKey: key.rawValue)
-    }
-    
-    func integer(forKey key: UserDefaultsKeyType) -> Int {
-        return integer(forKey: key.rawValue)
-    }
-    
-    // MARK: - Remove Method
-    
-    func removeObject(forKey key: UserDefaultsKeyType) {
-        removeObject(forKey: key.rawValue)
-    }
+  
+  // MARK: - Save Methods
+  
+  func set(_ value: String, forKey key: UserDefaultsKeyType) {
+    set(value, forKey: key.rawValue)
+  }
+  
+  func set(_ value: Bool, forKey key: UserDefaultsKeyType) {
+    set(value, forKey: key.rawValue)
+  }
+  
+  func set(_ value: Int, forKey key: UserDefaultsKeyType) {
+    set(value, forKey: key.rawValue)
+  }
+  
+  // MARK: - Retrieve Methods
+  
+  func string(forKey key: UserDefaultsKeyType) -> String? {
+    return string(forKey: key.rawValue)
+  }
+  
+  func bool(forKey key: UserDefaultsKeyType) -> Bool {
+    return bool(forKey: key.rawValue)
+  }
+  
+  func integer(forKey key: UserDefaultsKeyType) -> Int {
+    return integer(forKey: key.rawValue)
+  }
+  
+  // MARK: - Remove Method
+  
+  func removeObject(forKey key: UserDefaultsKeyType) {
+    removeObject(forKey: key.rawValue)
+  }
 }
