d2abdc07cb762a0179476b88e67feabf35dc6979
Zoe <mam07065@naver.com>
2024-08-15 21:18:28 +0900

    [Feat] Lottie 패키지 추가 및 스플래쉬 전환 로직

--- DDANZI_iOS/DDANZI_iOS/Application/SceneDelegate.swift
+++ DDANZI_iOS/DDANZI_iOS/Application/SceneDelegate.swift
@@ -17,12 +17,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
-    self.window = UIWindow(windowScene: windowScene)
     
-    let mainViewController = DdanziTabBarController()
-    let navigationController = UINavigationController(rootViewController: mainViewController)
-    self.window?.rootViewController = navigationController
-    self.window?.makeKeyAndVisible()
+    let window = UIWindow(windowScene: windowScene)
+    window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
+    
+    let splashVC = SplashViewController()
+    window.rootViewController = splashVC
+    window.makeKeyAndVisible()
+    
+    self.window = window
+    
+    let tabBarVC = DdanziTabBarController()
+    
+    // 스플래시 화면 2초 노출
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+      let navigationController = UINavigationController(rootViewController: tabBarVC)
+      
+      self.window?.rootViewController = navigationController
+    }
   }
   
   func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
