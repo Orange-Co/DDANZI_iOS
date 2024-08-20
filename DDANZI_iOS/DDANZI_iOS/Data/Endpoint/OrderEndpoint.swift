5f8403937545a29cf00be075a5cdb1fb41310ab8
Zoe <mam07065@naver.com>
2024-08-20 17:05:26 +0900

    [Feat/#34] 주문 Endpoint

--- DDANZI_iOS/DDANZI_iOS/Data/Endpoint/OrderEndpoint.swift
+++ DDANZI_iOS/DDANZI_iOS/Data/Endpoint/OrderEndpoint.swift
@@ -1,30 +1,65 @@
-////
-////  OrderEndpoint.swift
-////  DDANZI_iOS
-////
-////  Created by 이지희 on 8/2/24.
-////
 //
-//import Foundation
+//  OrderEndpoint.swift
+//  DDANZI_iOS
 //
-//import Moya
+//  Created by 이지희 on 8/2/24.
 //
-//enum OrderEndpoint {
-//}
-//
-//extension OrderEndpoint: BaseTargetType {
-//    var headers: Parameters? {
-//    }
-//    
-//    var path: String {
-//    }
-//    
-//    var method: Moya.Method {
-//    }
-//    
-//    var task: Moya.Task {
-//    }
-//    
-//    var validationType: ValidationType {
-//    }
-//}
+
+import Foundation
+
+import Moya
+
+enum OrderEndpoint {
+  case fetchOrderInfo(Int)
+  case executeOrder
+  case fetchOrderDetail(Int)
+}
+
+extension OrderEndpoint: BaseTargetType {
+  var headers: Parameters? {
+    switch self {
+    case .fetchOrderInfo:
+      return APIConstants.hasAccessTokenHeader
+    case .executeOrder:
+      return APIConstants.hasAccessTokenHeader
+    case .fetchOrderDetail:
+      return APIConstants.hasAccessTokenHeader
+    }
+  }
+  
+  var path: String {
+    switch self {
+    case .fetchOrderInfo(let id):
+      return "/api/v1/order/product/\(id)"
+    case .executeOrder:
+      return "/api/v1/order"
+    case .fetchOrderDetail(let id):
+      return "/api/v1/order/\(id)"
+    }
+  }
+  
+  var method: Moya.Method {
+    switch self {
+    case .fetchOrderInfo:
+      return .get
+    case .executeOrder:
+      return .post
+    case .fetchOrderDetail:
+      return .get
+    }
+  }
+  
+  var task: Moya.Task {
+    switch self {
+    case .fetchOrderInfo(let int):
+      return .requestPlain
+    case .executeOrder:
+      <#code#>
+    case .fetchOrderDetail(let int):
+      return .requestPlain
+    }
+  }
+  
+  var validationType: ValidationType {
+  }
+}
