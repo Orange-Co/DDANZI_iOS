f06a1e48d3af43c507862ae7d166287d51c25ad2
Zoe <mam07065@naver.com>
2024-08-20 17:04:41 +0900

    [Feat/#34] 주문 정보 DTO

---
+++ DDANZI_iOS/DDANZI_iOS/Data/DTO/Order/FetchOrderResponseDTO.swift
@@ -0,0 +1,29 @@
+//
+//  FetchOrderResponseDTO.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 8/20/24.
+//
+
+import Foundation
+
+// MARK: - FetchOrderResponseDTO
+struct FetchOrderResponseDTO: Codable {
+    let itemID, productName: String
+    let imgURL: String
+    let originPrice: Int
+    let addressInfo: AddressInfo
+    let discountPrice, charge, totalPrice: Int
+
+    enum CodingKeys: String, CodingKey {
+        case itemID = "itemId"
+        case productName
+        case imgURL = "imgUrl"
+        case originPrice, addressInfo, discountPrice, charge, totalPrice
+    }
+}
+
+// MARK: - AddressInfo
+struct AddressInfo: Codable {
+    let recipient, zipCode, address, recipientPhone: String
+}
