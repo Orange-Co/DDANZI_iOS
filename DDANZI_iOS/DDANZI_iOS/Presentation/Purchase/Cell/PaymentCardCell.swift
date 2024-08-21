89e8fd200dbdd16c853c1ce9e319b379ef1c0f99
Zoe <mam07065@naver.com>
2024-08-21 17:28:30 +0900

    feat:  #34 Project file

---
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/Cell/PaymentCardCell.swift
@@ -0,0 +1,8 @@
+//
+//  PaymentCardCell.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 8/20/24.
+//
+
+import Foundation
---
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/Model/Purchase.swift
@@ -0,0 +1,8 @@
+//
+//  Purchase.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 8/21/24.
+//
+
+import Foundation
