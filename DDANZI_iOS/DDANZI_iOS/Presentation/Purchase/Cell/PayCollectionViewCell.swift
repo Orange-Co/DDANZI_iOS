d6764b43dd445e8660c244401a1023cb78c94994
Zoe <mam07065@naver.com>
2024-08-21 17:33:34 +0900

    [Feat/#34] 결제 수단 설정뷰

--- DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/Cell/PayCollectionViewCell.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/Cell/PayCollectionViewCell.swift
@@ -7,6 +7,66 @@
 
 import UIKit
 
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+
 final class PayCollectionViewCell: UICollectionViewCell {
+  private let payment = ["신용/체크카드", "네이버페이", "카카오페이"]
+  private var selectedPayment = ""
+  
+  private let disposeBag = DisposeBag()
+  
+  private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.itemSize = .init(width: 103, height: 45)
+    flowLayout.minimumInteritemSpacing = 10
+    flowLayout.minimumLineSpacing = 10
+    $0.collectionViewLayout = flowLayout
+    $0.register(PaymentCardCell.self, forCellWithReuseIdentifier: PaymentCardCell.className)
+    $0.backgroundColor = .white
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUI()
+    setCollectionView()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setUI() {
+    setHierarchy()
+    setConstraints()
+  }
+  
+  private func setHierarchy() {
+    self.addSubviews(collectionView)
+  }
+  
+  private func setConstraints() {
+    collectionView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+  private func setCollectionView() {
+    let paymentObservable = Observable.just(payment)
     
+    paymentObservable
+      .bind(to: collectionView.rx.items(cellIdentifier: PaymentCardCell.className, cellType: PaymentCardCell.self)) { index, title, cell in
+        cell.configure(title: title)
+      }
+      .disposed(by: disposeBag)
+    
+    collectionView.delegate = self
+  }
+}
+
+extension PayCollectionViewCell: UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    self.selectedPayment = self.payment[indexPath.row]
+  }
 }
--- DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/Cell/PaymentCardCell.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/Cell/PaymentCardCell.swift
@@ -5,4 +5,50 @@
 //  Created by 이지희 on 8/20/24.
 //
 
-import Foundation
+import UIKit
+
+import Then
+import SnapKit
+
+final class PaymentCardCell: UICollectionViewCell {
+  
+  private let titleLabel = UILabel().then {
+    $0.font = .body5R14
+    $0.textColor = .gray2
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setUI() {
+    setHierarchy()
+    setConstraints()
+  }
+  
+  private func setHierarchy() {
+    contentView.makeCornerRound(radius: 10)
+    contentView.makeBorder(width: 1, color: .gray2)
+    contentView.addSubviews(titleLabel)
+  }
+  
+  private func setConstraints() {
+    titleLabel.snp.makeConstraints {
+      $0.center.equalToSuperview()
+    }
+  }
+  
+  func configure(title: String){
+    self.titleLabel.text = title
+  }
+  
+  func configureSelect() {
+    titleLabel.textColor = .gray4
+    contentView.makeBorder(width: 1, color: .gray4)
+  }
+}
