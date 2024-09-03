a46326d64239b7eff45000fe4a48938180bbabd0
Zoe <mam07065@naver.com>
2024-09-03 12:46:45 +0900

    [Feat/#34] 레이아웃 이슈 해결

--- DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/TableViewCell/TermsTableViewCell.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Purchase/TableViewCell/TermsTableViewCell.swift
@@ -11,15 +11,20 @@ import Then
 import SnapKit
 
 final class TermsTableViewCell: UITableViewCell {
+  private let containerView = UIView().then {
+    $0.backgroundColor = .white
+    $0.layer.cornerRadius = 5
+    $0.makeBorder(width: 1, color: .gray2)
+  }
   private let checkImageView = UIImageView().then {
-    $0.image = .icCheck
+    $0.image = .icCheck.withTintColor(.gray2)
   }
   private let titleLabel = UILabel().then {
     $0.font = .body4R16
-    $0.textColor = .gray4
+    $0.textColor = .gray2
   }
   
-
+  // MARK: - initializer
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     setUI()
@@ -29,19 +34,26 @@ final class TermsTableViewCell: UITableViewCell {
     fatalError("init(coder:) has not been implemented")
   }
   
+  
+  // MARK: - Layout
   private func setUI() {
     self.backgroundColor = .white
-    self.makeCornerRound(radius: 5)
-    self.makeBorder(width: 1, color: .gray3)
     setHierarchy()
     setConstraints()
   }
   
   private func setHierarchy() {
-    self.addSubviews(checkImageView, titleLabel)
+    contentView.addSubviews(containerView)
+    containerView.addSubviews(checkImageView, titleLabel)
   }
   
   private func setConstraints() {
+    containerView.snp.makeConstraints {
+      $0.height.equalTo(38)
+      $0.horizontalEdges.equalToSuperview()
+      $0.verticalEdges.equalToSuperview().inset(5)
+    }
+    
     checkImageView.snp.makeConstraints {
       $0.centerY.equalToSuperview()
       $0.leading.equalToSuperview().offset(10)
@@ -53,7 +65,15 @@ final class TermsTableViewCell: UITableViewCell {
     }
   }
   
-  func bindTitle(title: String) {
+  // MARK: - Method
+  func bindTitle(title: String, isSelected: Bool = false) {
     titleLabel.text = title
+    updateCheckBox(isSelected: isSelected)
+  }
+  
+  private func updateCheckBox(isSelected: Bool) {
+    checkImageView.image = isSelected ? UIImage(resource: .icCheck) : UIImage(resource: .icCheck).withTintColor(UIColor.gray2)
+    titleLabel.textColor = isSelected ? .black : .gray2
+    containerView.layer.borderColor = isSelected ? UIColor.black.cgColor : UIColor.gray2.cgColor
   }
 }
