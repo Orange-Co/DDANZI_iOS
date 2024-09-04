9c7d2acd40651e4235308290f7f5699d8fa9a1cf
Zoe <mam07065@naver.com>
2024-09-04 22:39:54 +0900

    [Feat/#36] DTO 수정

--- DDANZI_iOS/DDANZI_iOS/Data/DTO/Order/FetchOrderDetailResponseDTO.swift
+++ DDANZI_iOS/DDANZI_iOS/Data/DTO/Order/FetchOrderDetailResponseDTO.swift
@@ -9,22 +9,18 @@ import Foundation
 
 // MARK: - FetchOrderDetailResponseDTO
 struct FetchOrderDetailResponseDTO: Codable {
-    let orderID, orderStatus, productName, modifiedProductName: String
-    let imgURL: String
-    let originPrice: Int
-    let addressInfo: OrderAddressInfo
-    let paymentMethod, paidAt: String
-    let discountPrice, charge, totalPrice: Int
-
-    enum CodingKeys: String, CodingKey {
-        case orderID = "orderId"
-        case orderStatus, productName, modifiedProductName
-        case imgURL = "imgUrl"
-        case originPrice, addressInfo, paymentMethod, paidAt, discountPrice, charge, totalPrice
-    }
-}
-
-// MARK: - AddressInfo
-struct OrderAddressInfo: Codable {
-    let recipient, zipCode, address, recipientPhone: String
+  let orderID, orderStatus, productName: String
+  let imgURL: String
+  let originPrice: Int
+  let addressInfo: AddressInfo
+  let sellerNickname, paymentMethod: String
+  let paidAt: String?
+  let discountPrice, charge, totalPrice: Int
+  
+  enum CodingKeys: String, CodingKey {
+    case orderID = "orderId"
+    case orderStatus, productName
+    case imgURL = "imgUrl"
+    case originPrice, addressInfo, sellerNickname, paymentMethod, paidAt, discountPrice, charge, totalPrice
+  }
 }
