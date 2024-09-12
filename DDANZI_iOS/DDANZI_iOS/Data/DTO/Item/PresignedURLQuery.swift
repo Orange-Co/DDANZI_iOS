954aa2fb56128292b7e1c139ffb399915705b2c4
Zoe <mam07065@naver.com>
2024-09-12 15:23:38 +0900

    [Add/#38] PresignedURL DTO

---
+++ DDANZI_iOS/DDANZI_iOS/Data/DTO/Item/PresignedURLQuery.swift
@@ -0,0 +1,13 @@
+//
+//  PresignedURLRequestBody.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 9/9/24.
+//
+
+import Foundation
+
+struct PresignedURLQuery: Codable {
+  /// 확장자가 포함된 file이름
+  let fileName: String
+}
