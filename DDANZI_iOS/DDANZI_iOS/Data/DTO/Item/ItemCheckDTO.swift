68d552957f8fb2935d6d4c5850c3f71279b5e2d3
Zoe <mam07065@naver.com>
2024-09-12 15:23:09 +0900

    [Add/#38] 상품 확인 DTO

---
+++ DDANZI_iOS/DDANZI_iOS/Data/DTO/Item/ItemCheckDTO.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCheckDTO.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 9/9/24.
+//
+
+import Foundation
+
+struct ItemCheckDTO: Codable {
+  let productId: String
+  let productName: String
+  let imgUrl: String
+}
---
+++ DDANZI_iOS/DDANZI_iOS/Data/DTO/Item/ItemCheckRequestBody.swift
@@ -0,0 +1,12 @@
+//
+//  ItemCheckRequestBody.swift
+//  DDANZI_iOS
+//
+//  Created by 이지희 on 9/9/24.
+//
+
+import Foundation
+
+struct ItemCheckRequestBody: Codable {
+  let image_url: String
+}
