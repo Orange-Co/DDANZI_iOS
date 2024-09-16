4c0c3114e599fb319893f79027078a0d8feeeb10
Zoe <mam07065@naver.com>
2024-09-16 21:19:34 +0900

    [Feat/#38] 최상단 뷰컨트롤러

--- DDANZI_iOS/DDANZI_iOS/Common/Extension/UIView+.swift
+++ DDANZI_iOS/DDANZI_iOS/Common/Extension/UIView+.swift
@@ -158,4 +158,13 @@ extension UIView {
     }
   }
   
+  func findViewController() -> UIViewController? {
+    if let nextResponder = self.next as? UIViewController {
+      return nextResponder
+    } else if let nextResponder = self.next as? UIView {
+      return nextResponder.findViewController()
+    } else {
+      return nil
+    }
+  }
 }
