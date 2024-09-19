f874913e9fa4a40fb1f18ab424fc55aa1c989767
Zoe <mam07065@naver.com>
2024-09-19 23:18:33 +0900

    [Feat/#38] 상품 등록 뷰 구현

--- DDANZI_iOS/DDANZI_iOS/Presentation/Selling/RegisteItemCell.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Selling/RegisteItemCell.swift
@@ -17,6 +17,7 @@ final class RegisteItemCell: UICollectionViewCell {
   private let terms: [String] = ["서비스 이용 약관", "딴지 판매 가이드"]
   var selectedTerms = BehaviorRelay<[Bool]>(value: [false, false])
   var itemInfo = PublishRelay<itemConformedDTO>()
+  var dateString = BehaviorRelay<String>(value: "")
   private let disposeBag = DisposeBag()
   
   private let imageView = UIImageView()
@@ -25,7 +26,9 @@ final class RegisteItemCell: UICollectionViewCell {
     $0.text = "상품 정보"
     $0.textColor = .black
   }
-  private let productNameStackView = UIStackView()
+  private let productNameStackView = UIStackView().then {
+    $0.spacing = 10
+  }
   
   private let productNameLabel = UILabel().then {
     $0.font = .body5R14
@@ -38,7 +41,9 @@ final class RegisteItemCell: UICollectionViewCell {
     $0.textColor = .black
   }
   
-  private let kakaoPriceStackView = UIStackView()
+  private let kakaoPriceStackView = UIStackView().then {
+    $0.spacing = 10
+  }
   
   private let kakaoPriceLabel = UILabel().then {
     $0.font = .body5R14
@@ -51,7 +56,9 @@ final class RegisteItemCell: UICollectionViewCell {
     $0.textColor = .black
   }
   
-  private let priceStackView = UIStackView()
+  private let priceStackView = UIStackView().then {
+    $0.spacing = 10
+  }
   
   private let priceLabel = UILabel().then {
     $0.font = .body5R14
@@ -62,6 +69,10 @@ final class RegisteItemCell: UICollectionViewCell {
   private let priceContentLabel = UILabel().then {
     $0.font = .body4R16
     $0.textColor = .black
+  }
+  
+  private let priceSeperatorLine = UIView().then {
+    $0.backgroundColor = .gray4
   }
   
   private let dateTitleLabel = UILabel().then {
@@ -76,7 +87,7 @@ final class RegisteItemCell: UICollectionViewCell {
     $0.textColor = .black
   }
   
-  private let dateLabelButton = UIButton().then {
+  let dateLabelButton = UIButton().then {
     $0.setTitle("클릭해서 날짜를 선택하세요", for: .normal)
     $0.setTitleColor(.black, for: .normal)
     $0.backgroundColor = .white
@@ -84,6 +95,10 @@ final class RegisteItemCell: UICollectionViewCell {
     $0.layer.borderColor = UIColor.black.cgColor
     $0.layer.borderWidth = 1
     $0.clipsToBounds = true
+  }
+  
+  private let termSeperatorLine = UIView().then {
+    $0.backgroundColor = .gray4
   }
   
   private let fullAgreementButton = UIButton().then {
@@ -96,12 +111,14 @@ final class RegisteItemCell: UICollectionViewCell {
   
   private let termsTableView = UITableView().then {
     $0.register(TermsTableViewCell.self, forCellReuseIdentifier: TermsTableViewCell.className)
-    $0.rowHeight = 82
+    $0.rowHeight = 50
+    $0.separatorStyle = .none
   }
   
   override init(frame: CGRect) {
     super.init(frame: frame)
     setUI()
+    configureTableView()
     bindTableView()
   }
   
@@ -115,13 +132,17 @@ final class RegisteItemCell: UICollectionViewCell {
   }
   
   private func setHierarchy() {
-    self.addSubviews(imageView, productTitleLabel, productNameStackView, kakaoPriceStackView, priceStackView, dateTitleLabel, dateLabelButton, fullAgreementButton, termsTableView)
+    productNameStackView.addArrangedSubviews(productNameLabel, productNameContentLabel)
+    kakaoPriceStackView.addArrangedSubviews(kakaoPriceLabel, kakaoPriceContentLabel)
+    priceStackView.addArrangedSubviews(priceLabel, priceContentLabel)
+    self.addSubviews(imageView, productTitleLabel, productNameStackView, kakaoPriceStackView, priceStackView, dateTitleLabel, guideLabel, dateLabelButton, fullAgreementButton, termsTableView, termSeperatorLine, priceSeperatorLine)
   }
   
   private func setConstraints() {
     imageView.snp.makeConstraints {
       $0.top.equalToSuperview().inset(15)
-      $0.horizontalEdges.equalToSuperview().inset(20)
+      $0.centerX.equalToSuperview()
+      $0.size.equalTo(225.adjusted)
     }
     
     productTitleLabel.snp.makeConstraints {
@@ -144,8 +165,14 @@ final class RegisteItemCell: UICollectionViewCell {
       $0.horizontalEdges.equalToSuperview().inset(20)
     }
     
+    priceSeperatorLine.snp.makeConstraints {
+      $0.height.equalTo(1)
+      $0.horizontalEdges.equalToSuperview().inset(20)
+      $0.top.equalTo(priceStackView.snp.bottom).offset(32.adjusted)
+    }
+    
     dateTitleLabel.snp.makeConstraints {
-      $0.top.equalTo(priceStackView.snp.bottom).offset(15)
+      $0.top.equalTo(priceSeperatorLine.snp.bottom).offset(29.adjusted)
       $0.leading.equalToSuperview().offset(20)
     }
     
@@ -160,14 +187,20 @@ final class RegisteItemCell: UICollectionViewCell {
       $0.height.equalTo(50)
     }
     
-    fullAgreementButton.snp.makeConstraints {
-      $0.top.equalTo(dateLabelButton.snp.bottom).offset(20)
+    termSeperatorLine.snp.makeConstraints {
+      $0.height.equalTo(1)
       $0.horizontalEdges.equalToSuperview().inset(20)
+      $0.top.equalTo(dateLabelButton.snp.bottom).offset(20.adjusted)
+    }
+    
+    fullAgreementButton.snp.makeConstraints {
+      $0.top.equalTo(termSeperatorLine.snp.bottom).offset(30.adjusted)
+      $0.leading.equalToSuperview().inset(20)
     }
     
     termsTableView.snp.makeConstraints {
-      $0.top.equalTo(fullAgreementButton.snp.bottom).offset(10)
-      $0.horizontalEdges.equalToSuperview()
+      $0.top.equalTo(fullAgreementButton.snp.bottom).offset(35.adjusted)
+      $0.leading.trailing.bottom.equalToSuperview()
     }
   }
   
@@ -184,10 +217,10 @@ final class RegisteItemCell: UICollectionViewCell {
   private func bindTableView() {
     itemInfo
       .subscribe(with: self) { owner, info in
-        self.imageView.setImage(with: info.imgUrl)
+        self.imageView.setImage(with: info.imgURL)
         self.productNameContentLabel.text = info.productName
         self.kakaoPriceContentLabel.text = info.originPrice.toKoreanWon()
-        self.priceLabel.text = info.salePrice.toKoreanWon()
+        self.priceContentLabel.text = info.salePrice.toKoreanWon()
       }
       .disposed(by: disposeBag)
     
@@ -208,6 +241,13 @@ final class RegisteItemCell: UICollectionViewCell {
         self?.updateFullAgreementButtonState(termsSelected)
       })
       .disposed(by: disposeBag)
+    
+    // dateLabelButton을 눌렀을 때 DatePicker 표시
+    dateLabelButton.rx.tap
+      .subscribe(onNext: { [weak self] in
+        self?.showDatePicker()
+      })
+      .disposed(by: disposeBag)
   }
   
   // 전체 약관 선택 및 해제 로직
@@ -221,6 +261,48 @@ final class RegisteItemCell: UICollectionViewCell {
     let allSelected = termsSelected.allSatisfy { $0 }
     fullAgreementButton.isSelected = allSelected
   }
+  
+  private func showDatePicker() {
+    // DatePicker를 포함한 AlertController 생성
+    let alert = UIAlertController(title: "\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+    
+    // DatePicker 생성
+    let datePicker = UIDatePicker()
+    datePicker.datePickerMode = .date
+    datePicker.preferredDatePickerStyle = .wheels // iOS 14 이상에서 wheels 스타일
+    datePicker.frame = CGRect(x: 0, y: 0, width: alert.view.frame.width - 20, height: 150)
+    
+    // DatePicker를 AlertController에 추가
+    alert.view.addSubview(datePicker)
+    
+    // 선택 버튼
+    let selectAction = UIAlertAction(title: "선택", style: .default) { _ in
+      let selectedDate = datePicker.date
+      self.updateDateLabel(with: selectedDate)
+    }
+    
+    // 취소 버튼
+    let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+    
+    alert.addAction(selectAction)
+    alert.addAction(cancelAction)
+    
+    // ViewController에 AlertController를 표시
+    if let viewController = self.findViewController() {
+      viewController.present(alert, animated: true, completion: nil)
+    }
+  }
+  
+  // 날짜를 버튼에 반영하는 메서드
+  private func updateDateLabel(with date: Date) {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd" // 원하는 날짜 형식
+    let dateString = formatter.string(from: date)
+    
+    self.dateString.accept(dateString)
+    self.dateLabelButton.setTitle(dateString, for: .normal)
+  }
+  
 }
 
 extension RegisteItemCell: UITableViewDataSource {
--- DDANZI_iOS/DDANZI_iOS/Presentation/Selling/RegisteItemViewController.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Selling/RegisteItemViewController.swift
@@ -16,26 +16,45 @@ final class RegisteItemViewController: UIViewController {
   
   private let disposeBag = DisposeBag()
   let itemInfo = PublishRelay<itemConformedDTO>()
-  var info: itemConformedDTO = .init(productId: "", productName: "", originPrice: 0, salePrice: 0, isAccounExist: true, imgUrl: "")
+  var info: itemConformedDTO = .init(productID: "", productName: "", imgURL: "", originPrice: 0, salePrice: 0, isAccountExist: false)
   
   // MARK: - UI
   private let navigationBar = CustomNavigationBarView(navigationBarType: .cancel, title: "판매하기")
-  private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
+  private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
     $0.backgroundColor = .white
     $0.register(RegisteItemCell.self, forCellWithReuseIdentifier: RegisteItemCell.className)
+  }
+  private let buttonView = UIView().then {
+    $0.backgroundColor = .white
+  }
+  private let registeButton = DdanziButton(title: "판매하기", enable: false)
+  
+  init(info: itemConformedDTO) {
+    self.info = info
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    bind()
+    configureCollectionView()
+    
+    setUI()
   }
   
   private func setUI() {
+    view.backgroundColor = .white
     setHierarchy()
     setConstraints()
   }
   
   private func setHierarchy() {
-    view.addSubviews(navigationBar, collectionView)
+    view.addSubviews(navigationBar, collectionView, buttonView)
+    buttonView.addSubview(registeButton)
   }
   
   private func setConstraints() {
@@ -48,6 +67,17 @@ final class RegisteItemViewController: UIViewController {
       $0.top.equalTo(navigationBar.snp.bottom)
       $0.leading.trailing.bottom.equalToSuperview()
     }
+    
+    buttonView.snp.makeConstraints {
+      $0.bottom.leading.trailing.equalToSuperview()
+      $0.height.equalTo(92)
+    }
+    
+    registeButton.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(20)
+      $0.leading.trailing.equalToSuperview().inset(20)
+      $0.height.equalTo(50)
+    }
   }
   
   private func bind() {
@@ -57,10 +87,46 @@ final class RegisteItemViewController: UIViewController {
         owner.collectionView.reloadData()
       }
       .disposed(by: disposeBag)
+    
+    registeButton.rx.tap
+      .subscribe(with: self) { owner, _ in
+        guard let cell = owner.collectionView.cellForItem(at: IndexPath(item: 0, section: 0)) as? RegisteItemCell else {
+          print("셀 또는 dataButton Label을 찾을 수 없음")
+          return
+        }
+        let dueDate = cell.dateString.value
+        owner.registeItem(due: dueDate)
+      }
+      .disposed(by: disposeBag)
+    
+    navigationBar.cancelButtonTap
+      .subscribe(with: self) { owner, _ in
+        owner.navigationController?.popViewController(animated: true)
+      }
+      .disposed(by: disposeBag)
   }
   
   private func configureCollectionView() {
     collectionView.dataSource = self
+    collectionView.delegate = self
+  }
+  
+  private func registeItem(due: String) {
+    let body = RegisteItemBody(productId: info.productID, productName: info.productName, receivedDate: due, registeredImage: info.imgURL)
+    Providers.ItemProvider.request(target: .registeItem(body: body), instance: BaseResponse<RegisteItemDTO>.self) { response in
+      guard let data = response.data else { return }
+      let registeCompleteVC = RegisteCompleteViewController(response: data)
+      let pushVC = PushSettingViewController(isSelling: true, orderId: "", response: data)
+      PermissionManager.shared.checkPermission(for: .notification)
+        .bind(with: self, onNext: { owner, isAllow in
+          if isAllow {
+            self.navigationController?.pushViewController(registeCompleteVC, animated: true)
+          } else  {
+            self.navigationController?.pushViewController(pushVC, animated: true)
+          }
+        })
+        .disposed(by: self.disposeBag)
+    }
   }
 }
 
@@ -72,9 +138,27 @@ extension RegisteItemViewController: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RegisteItemCell.className, for: indexPath) as? RegisteItemCell else { return UICollectionViewCell() }
     cell.configure(info: self.info)
+    
+    // 날짜와 전체 동의 여부 확인
+    Observable.combineLatest(cell.selectedTerms, cell.dateString)
+      .map { termsSelected, date in
+        // 모든 약관이 동의됐고, 날짜가 선택됐는지 확인
+        let allSelected = termsSelected.allSatisfy { $0 }
+        return allSelected && !date.isEmpty
+      }
+      .bind(onNext: { isAllow in
+        self.registeButton.setEnable()
+      })
+      .disposed(by: disposeBag)
+    
+    
     return cell
     
   }
-  
-  
+}
+
+extension RegisteItemViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: collectionView.frame.width, height: 850.adjusted) // 원하는 크기 설정
+  }
 }
