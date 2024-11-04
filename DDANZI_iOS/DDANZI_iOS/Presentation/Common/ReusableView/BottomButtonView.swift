c6bff05ab00c3bf0a0d3312c7d97781f67598835
Zoe <mam07065@naver.com>
2024-11-05 02:32:53 +0900

    [Feat/#52] 상세 화면 하트 반영

--- DDANZI_iOS/DDANZI_iOS/Presentation/Common/ReusableView/BottomButtonView.swift
+++ DDANZI_iOS/DDANZI_iOS/Presentation/Common/ReusableView/BottomButtonView.swift
@@ -13,86 +13,91 @@ import RxSwift
 import RxCocoa
 
 class BottomButtonView: UIView {
-    let backgroundView = UIView().then {
-        $0.backgroundColor = .white
-        $0.addShadow(offset: .init(width: 1, height: 1))
-    }
-    
-    let button = UIButton().then {
-        $0.backgroundColor = .black
-        $0.makeCornerRound(radius: 5)
-        $0.setTitleColor(.white, for: .normal)
-        $0.titleLabel?.font = .body3Sb16
-    }
-    
-    let heartButton = UIButton().then {
-        $0.setImage(.icBlackEmptyHeart, for: .normal)
-      $0.setImage(.icFillHeartYellow, for: .selected)
-    }
-    
-    let heartCountLabel = UILabel().then {
-        $0.textColor = .black
-        $0.font = .body6M12
-    }
+  let backgroundView = UIView().then {
+    $0.backgroundColor = .white
+    $0.addShadow(offset: .init(width: 1, height: 1))
+  }
   
-    private let disposeBag = DisposeBag()
-    private let heartButtonSubject = PublishSubject<Void>()
-    var heartButtonTap: Observable<Void> { heartButtonSubject.asObservable() }
-    
+  let button = UIButton().then {
+    $0.backgroundColor = .black
+    $0.makeCornerRound(radius: 5)
+    $0.setTitleColor(.white, for: .normal)
+    $0.titleLabel?.font = .body3Sb16
+  }
+  
+  let heartButton = UIButton().then {
+    $0.setImage(.icBlackEmptyHeart, for: .normal)
+    $0.setImage(.icFillHeartYellow, for: .selected)
+  }
+  
+  let heartCountLabel = UILabel().then {
+    $0.textColor = .black
+    $0.font = .body6M12
+  }
+  
+  private let disposeBag = DisposeBag()
+  private let heartButtonSubject = PublishSubject<Void>()
+  var heartButtonTap: Observable<Void> { heartButtonSubject.asObservable() }
+  
   init(buttonText: String, heartCount: Int = 0, isEnable: Bool = false) {
-        super.init(frame: .zero)
-        self.button.setTitle(buttonText, for: .normal)
-        self.heartCountLabel.text = "\(heartCount)"
-        self.button.isEnabled = isEnable
-        self.button.backgroundColor = isEnable ? .black : .gray2
-        setUI()
-        bind()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setUI() {
-        self.backgroundColor = .white
-      self.addShadow(offset: .init(width: 0, height: 2), opacity: 0.3)
-        setHierarchy()
-        setConstraints()
-    }
-    
-    private func setHierarchy() {
-        self.addSubviews(heartButton,
-                         heartCountLabel,
-                         button)
-    }
-    
-    private func setConstraints() {
-        self.snp.makeConstraints {
-            $0.height.equalTo(82)
-        }
-        
-        heartButton.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(23)
-            $0.leading.equalToSuperview().offset(32)
-            $0.width.equalTo(23)
-        }
-        
-        heartCountLabel.snp.makeConstraints {
-            $0.top.equalTo(heartButton.snp.bottom).offset(3)
-            $0.centerX.equalTo(heartButton.snp.centerX)
-        }
-        
-        button.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(16)
-            $0.leading.equalTo(heartButton.snp.trailing).offset(20)
-            $0.trailing.equalToSuperview().inset(23)
-            $0.height.equalTo(50)
-        }
-    }
+    super.init(frame: .zero)
+    self.button.setTitle(buttonText, for: .normal)
+    self.heartCountLabel.text = "\(heartCount)"
+    self.button.isEnabled = isEnable
+    self.button.backgroundColor = isEnable ? .black : .gray2
+    setUI()
+    bind()
+  }
   
-    private func bind() {
-      heartButton.rx.tap
-        .bind(to: heartButtonSubject)
-        .disposed(by: disposeBag)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setUI() {
+    self.backgroundColor = .white
+    self.addShadow(offset: .init(width: 0, height: 2), opacity: 0.3)
+    setHierarchy()
+    setConstraints()
+  }
+  
+  private func setHierarchy() {
+    self.addSubviews(heartButton,
+                     heartCountLabel,
+                     button)
+  }
+  
+  private func setConstraints() {
+    self.snp.makeConstraints {
+      $0.height.equalTo(82)
     }
+    
+    heartButton.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(23)
+      $0.leading.equalToSuperview().offset(32)
+      $0.width.equalTo(23)
+    }
+    
+    heartCountLabel.snp.makeConstraints {
+      $0.top.equalTo(heartButton.snp.bottom).offset(3)
+      $0.centerX.equalTo(heartButton.snp.centerX)
+    }
+    
+    button.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(16)
+      $0.leading.equalTo(heartButton.snp.trailing).offset(20)
+      $0.trailing.equalToSuperview().inset(23)
+      $0.height.equalTo(50)
+    }
+  }
+  
+  private func bind() {
+    heartButton.rx.tap
+      .bind(to: heartButtonSubject)
+      .disposed(by: disposeBag)
+  }
+  
+  func configure(heartCount: Int, isInterest: Bool) {
+    self.heartCountLabel.text = "\(heartCount)"
+    self.heartButton.isSelected = isInterest
+  }
 }
